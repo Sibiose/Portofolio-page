d382e2ceeed1b5e7070d14538cb9a8fa6a8c7514
Sibiose <89642007+Sibiose@users.noreply.github.com>
2022-01-31 10:18:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+window.addEventListener("load", (event) => {
+  const header = document.querySelector("header");
+  const scrollTop = document.querySelector("#scroll-top");
+  const nav = document.querySelector("nav");
+  const navLinks = [...document.querySelectorAll(".nav-link")];
+  const logo = document.querySelector(".logo");
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const navListModal = document.querySelector(".nav-ul-modal");
+
+  window.onscroll = function () {
+    scrollTopFun();
+    navBarScroll();
+  };
+
+  function scrollTopFun() {
+    if (
+      window.innerWidth > 450 &&
+      (document.body.scrollTop > 800 ||
+        document.documentElement.scrollTop > 800)
+    ) {
+      scrollTop.style.display = "block";
+    } else {
+      scrollTop.style.display = "none";
+    }
+  }
+
+  function navBarScroll() {
+    if (
+      document.body.scrollTop > 700 ||
+      document.documentElement.scrollTop > 700
+    ) {
+      nav.style.backgroundColor = "#04323e";
+      nav.style.height = "2.5rem";
+
+      navListModal.style.top = "1.5rem";
+    } else {
+      if (menuIsOpen != true) nav.style.backgroundColor = "transparent";
+      nav.style.height = "auto";
+
+      navListModal.style.top = "2rem";
+    }
+  }
+
+  header.scrollIntoView();
+
+  let menuIsOpen = false;
+
+  scrollTop.addEventListener("click", () => {
+    header.scrollIntoView({ behavior: "smooth" });
+  });
+
+  hamburgerMenu.addEventListener("click", () => {
+    navListModal.classList.toggle("open-modal");
+    if (!menuIsOpen) {
+      menuIsOpen = true;
+      nav.style.backgroundColor = "#04323e";
+    } else {
+      menuIsOpen = false;
+      if (
+        document.body.scrollTop > 700 ||
+        document.documentElement.scrollTop > 700
+      ) {
+        return;
+      } else {
+        nav.style.backgroundColor = "transparent";
+      }
+    }
+  });
+});
